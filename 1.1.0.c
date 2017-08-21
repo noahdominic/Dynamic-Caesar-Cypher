0872361862fb3429b79314c934c13894cb8062c6
Noah Dominic Miranda Silvio <ndsilvio@outlook.ph>
2017-08-21 18:51:41 +0800

    Update 1.1.0.c

--- 1.1.0.c
+++ 1.1.0.c
@@ -2,20 +2,32 @@
 
 char* encrypt(char*, int);
 int main(void) {
-    char phrase_raw[100], encrypted[100];
+    char phrase_raw[100];
     char* phrase_encrypted;
-    unsigned int shift = 0, i =0;
-    printf("Hello. This is a Dynamic Caesar Generator. \nPlease enter the initial shift: ");
-    scanf("%i", &shift);
-    printf("Please enter the text to be encrypted: ");
-    scanf(" %[^\n]s", phrase_raw);
-    phrase_encrypted = encrypt(phrase_raw, shift);
-    printf("Here is your encrypted text          : %s", phrase_encrypted);
+    unsigned int shift = 0, i = 0;
+    unsigned int taskChoice = 0;
+    printf("Hello! \nThis is a Dynamic Caesar Cypher Generator. What would you like to do? \n1 - Encrypt\n2 - Decrypt\nOther - Exit");
+    scanf("%i", &taskChoice);
+    if(taskChoice == 1){
+        printf("Please enter the initial shift: ");
+        scanf("%i", &shift);
+        printf("Please enter the text to be encrypted: ");
+        scanf(" %[^\n]s", phrase_raw);
+        phrase_encrypted = encrypt(phrase_raw, shift);
+        printf("Here is your encrypted text          : %s", phrase_encrypted);
+        free(phrase_encrypted);
+    } else if(taskChoice == 2) {
+        //TODO insert call for decrypt function here
+    } else {
+        exit(EXIT_SUCCESS);
+    }
     return 0;
 }
 
+// accepts a clean, raw phrase to be encrypted and an initial shift value, returns pointer to the encrypted phrase
 char* encrypt(char *raw, int shift) {
     char* encrypted;
+    encrypted = (char *)malloc(strlen(raw) + 1);
     int i = 0;
     do {
         if(raw[i] >= 'A' && raw[i] <= 'Z'){
